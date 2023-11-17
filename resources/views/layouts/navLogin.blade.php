e4465955bf80cb6ca4931f25a2f220f9aae5deda
t1-nhd <xelam654@gmail.com>
2023-11-17 18:35:08 +0700

    Profile vip pro
    News:
    - Cập nhật profile
    - Sửa lỗi {$user->$xxx}
    - Alert Cập nhật thành công (chưa được)

---
+++ resources/views/layouts/navLogin.blade.php
@@ -0,0 +1,26 @@
+<div class="h-[88.8px]">
+    <nav class="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="{{ route('index') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="/storage/images/logo.png" class="h-14" alt="Logo">
+            </a>
+            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <a href="{{ route('cartIndex') }}" type="button" class="cursor-pointer duration-300 text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center">
+                </a>
+                <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
+                    <span class="sr-only">Mở Menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+                    <li>
+                        <a href="{{ route('index') }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Trang Chủ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
